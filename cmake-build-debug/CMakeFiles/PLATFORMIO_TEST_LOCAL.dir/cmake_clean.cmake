eba842113e27596dcb7c26a060c9fb35073f347c
Generoso Martello <generoso.martello@gmail.com>
2019-01-27 18:28:14 +0100

    updating ignore list

--- cmake-build-debug/CMakeFiles/PLATFORMIO_TEST_LOCAL.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PLATFORMIO_TEST_LOCAL"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PLATFORMIO_TEST_LOCAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
