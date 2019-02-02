548efe7a37460c773a45f434d1cbf93b0664e57e
Generoso Martello <generoso.martello@gmail.com>
2019-02-02 13:10:00 +0100

    adding CI / fix tinyxml2 dependency

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,9 @@
  */
 
 #include <Arduino.h>
+
+#include <ESP8266HTTPClient.h> // this is required or Travis CI build will break
+
 #include <net/NetManager.h>
 #include <io/Logger.h>
 #include <scripting/ProgramEngine.h>
